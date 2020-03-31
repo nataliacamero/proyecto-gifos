5cddbcdb604ed7e79b05f65ba82e78acce83cdc5
John Doe <nataliacameroc@gmail.com>
2020-03-31 04:44:23 -0500

    Haciend cabios de tema

--- js/cambiar_tema-index.js
+++ js/cambiar_tema-index.js
@@ -1,37 +1,42 @@
-let anclaDay = document.getElementsByTagName("a")[4].onclick = function() {cambiarTema("day")};
+function toogleTema() {
+
+    let anclaDay = document.getElementsByTagName("a")[4].onclick = function() {cambiarTema("day")};
+
+    let anclaNight = document.getElementsByTagName("a")[5].onclick = function() {cambiarTema("night")};
 
 
-let anclaNight = document.getElementsByTagName("a")[5].onclick = function() {cambiarTema("night")};
-
-let likCrerGuifo = document.getElementsByTagName("p")[0].onclick = function() {setTema()};
+    let likCrerGuifo = document.getElementsByTagName("a")[1].onclick = function() {setTema()};
 
 
 
-function cambiarTema(theme){
-    if(!sessionStorage.getItem("tema") || theme === "day") {
-        sessionStorage.setItem("tema","estilos/sailor-day.css");
-        document.getElementsByTagName("img")[0].setAttribute("src", "./assets/gifOF_logo.png" );
-    } else {
-        sessionStorage.setItem("tema","estilos/sailor-night.css");
-        document.getElementsByTagName("img")[0].setAttribute("src", "./assets/gifOF_logo_dark.png" );
+    function cambiarTema(theme){
+        if(!sessionStorage.getItem("tema") || theme === "day") {
+            sessionStorage.setItem("tema","estilos/sailor-day.css");
+            document.getElementsByTagName("img")[0].setAttribute("src", "./assets/gifOF_logo.png" );
+        } else {
+            sessionStorage.setItem("tema","estilos/sailor-night.css");
+            document.getElementsByTagName("img")[0].setAttribute("src", "./assets/gifOF_logo_dark.png" );
+        }
+        let link = document.getElementsByTagName("link")[0].setAttribute("href", sessionStorage.getItem("tema"));
+        
+        
     }
-    let link = document.getElementsByTagName("link")[0].setAttribute("href", sessionStorage.getItem("tema"));
-    
-     
+
+    console.log(document.getElementsByTagName("img")[0]);
+    function setTema() {
+        if (!sessionStorage.getItem("tema")) {
+            sessionStorage.setItem("tema", "estilos/sailor-day.css");       
+        }
+        
+        if(sessionStorage.getItem("tema") === "estilos/sailor-day.css"){
+            document.getElementsByTagName("img")[0].setAttribute("src", "./assets/gifOF_logo.png" );
+        } else {
+            document.getElementsByTagName("img")[0].setAttribute("src", "./assets/gifOF_logo_dark.png" );
+        }  
+        document.getElementsByTagName("link")[0].setAttribute("href", sessionStorage.getItem("tema"));
+    }
+    setTema();
+
 }
 
-
-function setTema() {
-    if (!sessionStorage.getItem("tema")) {
-        sessionStorage.setItem("tema", "estilos/sailor-day.css");       
-    }
-    
-    if(sessionStorage.getItem("tema") === "estilos/sailor-day.css"){
-        document.getElementsByTagName("img")[0].setAttribute("src", "./assets/gifOF_logo.png" );
-    } else {
-        document.getElementsByTagName("img")[0].setAttribute("src", "./assets/gifOF_logo_dark.png" );
-    }  
-    document.getElementsByTagName("link")[0].setAttribute("href", sessionStorage.getItem("tema"));
-}
-setTema();
-
+toogleTema();
