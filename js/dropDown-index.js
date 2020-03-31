c1822948dd457b9584e7dc9c4c27442b0e525799
John Doe <nataliacameroc@gmail.com>
2020-03-31 03:33:11 -0500

    Cambios de sailor

--- js/dropDown-index.js
+++ js/dropDown-index.js
@@ -48,13 +48,24 @@ window.onclick = function(event) {
                 openDropdown.classList.remove('mostrar');
             }
         }
-        document.querySelectorAll(".boton_buscar")[0].style.background = "#F7C9F3";
-        document.querySelectorAll(".boton_buscar")[0].style.backgroundImage = "url(../assets/lupa.svg)";
-        document.querySelectorAll(".boton_buscar")[0].style.backgroundRepeat= "no-repeat";
-        document.querySelectorAll(".boton_buscar")[0].style.backgroundPosition = "left 5px top 10px";
-        document.querySelectorAll(".boton_buscar")[0].style.boxShadow = "inset -1px -1px 0 0 #997D97, inset 1px 1px 0 0 #FFFFFF";
-        document.querySelectorAll(".boton_buscar")[0].style.border = "1px solid #110038";
-        document.querySelectorAll(".boton-texto--buscar")[0].style.color = "#110038";
+       
+        if (false) {
+            document.querySelectorAll(".boton_buscar")[0].style.background = "#F7C9F3";
+            document.querySelectorAll(".boton_buscar")[0].style.backgroundImage = "url(../assets/lupa.svg)";
+            document.querySelectorAll(".boton_buscar")[0].style.backgroundRepeat= "no-repeat";
+            document.querySelectorAll(".boton_buscar")[0].style.backgroundPosition = "left 5px top 10px";
+            document.querySelectorAll(".boton_buscar")[0].style.boxShadow = "inset -1px -1px 0 0 #997D97, inset 1px 1px 0 0 #FFFFFF";
+            document.querySelectorAll(".boton_buscar")[0].style.border = "1px solid #110038";
+            document.querySelectorAll(".boton-texto--buscar")[0].style.color = "#110038";     
+        } else {
+            document.querySelectorAll(".boton_buscar")[0].style.background = "#EE3EFE";
+            document.querySelectorAll(".boton_buscar")[0].style.backgroundImage = "url(../assets/lupa_light.svg)";
+            document.querySelectorAll(".boton_buscar")[0].style.backgroundRepeat= "no-repeat";
+            document.querySelectorAll(".boton_buscar")[0].style.backgroundPosition = "left 5px top 10px";
+            document.querySelectorAll(".boton_buscar")[0].style.boxShadow = "inset -1px -1px 0 0 #A72CB3, inset 1px 1px 0 0 #FFFFFF";
+            document.querySelectorAll(".boton_buscar")[0].style.border = "1px solid #110038";
+            document.querySelectorAll(".boton-texto--buscar")[0].style.color = "#ffffff";
+        }
 
 
 
